702647a423213f30e819163e752dfe7e98416c87
Kurbanou <kurbanou.faridun@gmail.com>
2025-06-07 21:07:26 +0300

    feat:header modify

---
+++ app.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector(".header__burger");
+const header = document.querySelector("header");
+burger.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
