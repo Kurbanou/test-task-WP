81731987bb4c7406c087b61905461493a997b5b2
Kurbanou <kurbanou.faridun@gmail.com>
2025-06-08 21:05:32 +0300

    feat:header finish

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const burger = document.querySelector(".header__burger");
 const header = document.querySelector("header");
+const linkHeader = header.querySelectorAll("li");
+
 burger.addEventListener("click", () => {
   header.classList.toggle("active");
+  console.log(linkHeader);
+});
+
+linkHeader.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.classList.toggle("active");
+  });
 });
